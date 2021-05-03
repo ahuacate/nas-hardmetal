6defe7fe04a389f46edac14e7a182a4c000226a2
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2021-05-03 12:50:08 +0700

    Create test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# ----------------------------------------------------------------------------------
+# Filename:     pve_medialab_ct_kodi-rsync_serveraddclient.sh
+# Description:  This script is for creating a Kodi-Rsync Client
+# ----------------------------------------------------------------------------------
+
+#---- Bash command to run script ---------------------------------------------------
+
+#bash -c "$(wget -qLO - https://raw.githubusercontent.com/ahuacate/pve-medialab/master/scripts/pve_medialab_ct_kodi-rsync_serveraddclient.sh)"
+
+#---- Source -----------------------------------------------------------------------
+
+# Set Temp Folder
+if [ -z "${TEMP_DIR+x}" ]; then
+  TEMP_DIR=$(mktemp -d)
+  cd $TEMP_DIR >/dev/null
+else
+  if [ $(pwd -P) != $TEMP_DIR ]; then
+    cd $TEMP_DIR >/dev/null
+  fi
+fi
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+PVE_SOURCE="$DIR/../../common/pve/source"
+echo "Here is DIR: ${DIR}"
+PVE_SOURCE="$DIR/../../common/pve/source"
+echo "Here is DIR: ${PVE_SOURCE}"
