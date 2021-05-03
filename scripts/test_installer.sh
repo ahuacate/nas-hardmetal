be65aa0b4c6a751a88e41d85dc7a35a5a1f2f5b2
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2021-05-03 12:49:27 +0700

    Update and rename test.sh to test_installer.sh

--- scripts/test_installer.sh
+++ scripts/test_installer.sh
@@ -9,6 +9,22 @@
 #bash -c "$(wget -qLO - https://raw.githubusercontent.com/ahuacate/pve-medialab/master/scripts/pve_medialab_ct_kodi-rsync_serveraddclient.sh)"
 
 #---- Source -----------------------------------------------------------------------
+
+# Set Temp Folder
+if [ -z "${TEMP_DIR+x}" ]; then
+  TEMP_DIR=$(mktemp -d)
+  cd $TEMP_DIR >/dev/null
+else
+  if [ $(pwd -P) != $TEMP_DIR ]; then
+    cd $TEMP_DIR >/dev/null
+  fi
+fi
+
+wget https://github.com/ahuacate/nas-oem-setup/archive/refs/heads/master.tar.gz
+tar -zxvf master.tar.gz
+
+source ${TEMP_DIR}/nas-oem-setup-master/scripts/test.sh
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 PVE_SOURCE="$DIR/../../common/pve/source"
 echo "Here is DIR: ${DIR}"
